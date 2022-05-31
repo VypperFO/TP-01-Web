da9b1543b00c487b2e0cd9917795c222595fc4b6
VypperFO <felixlatulip@gmail.com>
2022-05-31 18:57:03 -0400

    choses

--- js/script.js
+++ js/script.js
@@ -18,9 +18,17 @@ document.getElementById("date").innerHTML = dateTime;
 // Changer langue
 
 function changeFrench() {
-  window.alert("Something happened in french");
+  document.getElementById("support-menu").innerHTML = "support.";
+  document.getElementById("monde-menu").innerHTML = "monde.";
+  document.getElementById("economie-menu").innerHTML = "économie.";
+  document.getElementById("russie-menu").innerHTML = "russie.";
+  document.getElementById("ukraine-menu").innerHTML = "ukraine.";
 }
 
 function changeEnglish() {
-  window.alert("Something happened in english");
+  document.getElementById("support-menu").innerHTML = "support.";
+  document.getElementById("monde-menu").innerHTML = "world.";
+  document.getElementById("economie-menu").innerHTML = "economy.";
+  document.getElementById("russie-menu").innerHTML = "russia.";
+  document.getElementById("ukraine-menu").innerHTML = "ukraine.";
 }
