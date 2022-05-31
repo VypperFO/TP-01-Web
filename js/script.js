1ffc835c4b2b52d69594bbc612ab741cab9a468d
VypperFO <felixlatulip@gmail.com>
2022-05-31 17:38:25 -0400

    francais corriger

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 const currentDate = new Date();
 const dateTime =
-    "Date / Heure: " +
-    currentDate.getDay() +
-    "/" +
-    currentDate.getMonth() +
-    "/" +
-    currentDate.getFullYear() +
-    " @ " +
-    currentDate.getHours() +
-    ":" +
-    currentDate.getMinutes() +
-    ":" +
-    currentDate.getSeconds();
+  "Date / Heure: " +
+  currentDate.getDay() +
+  "/" +
+  currentDate.getMonth() +
+  "/" +
+  currentDate.getFullYear() +
+  " @ " +
+  currentDate.getHours() +
+  ":" +
+  currentDate.getMinutes() +
+  ":" +
+  currentDate.getSeconds();
 
 document.getElementById("date").innerHTML = dateTime;
+
+// Changer langue
+
+function changeFrench() {
+  window.alert("Something happened in french");
+}
+
+function changeEnglish() {
+  window.alert("Something happened in english");
+}
